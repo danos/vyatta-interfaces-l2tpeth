2b5229dc43c19f0e8359bd5715726c668d7cfec8
Mike Manning <mmanning@vyatta.att-mail.com>
2020-10-30 09:15:35 +0000

    Harden l2tpeth script
    Avoid risk of code injection due to shell expansion of strings by
    passing the arguments as an array for the system call.
    
    VRVDR-53450

--- scripts/vyatta-l2tpeth.pl
+++ scripts/vyatta-l2tpeth.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl
 
 #
-# Copyright (c) 2019 AT&T Intellectual Property. All Rights Reserved.
+# Copyright (c) 2019-2020 AT&T Intellectual Property. All Rights Reserved.
 # Copyright (c) 2014-2016, Brocade Communications Systems, Inc.
 # 
 # SPDX-License-Identifier: GPL-2.0-only
@@ -38,12 +38,12 @@ my $local_cookie = $cfg->returnValue('l2tp-session local-cookie');
 my $peer_cookie = $cfg->returnValue('l2tp-session remote-cookie');
 my $local_udp = $cfg->returnValue('l2tp-session local-udp-port');
 my $peer_udp = $cfg->returnValue('l2tp-session remote-udp-port');
-my $port = " ";
-my $cookie = " ";
+my $port = [];
+my $cookie = [];
 
-$port = "udp_sport $local_udp udp_dport $peer_udp"
+$port = [ "udp_sport", $local_udp, "udp_dport", $peer_udp ]
     if defined($local_udp);
-$cookie = "cookie $local_cookie peer_cookie $peer_cookie"
+$cookie = [ "cookie", $local_cookie, "peer_cookie", $peer_cookie ]
     if defined($local_cookie and $peer_cookie);
 
 if ( $action eq 'SET' ) {
@@ -84,12 +84,13 @@ if ( $action eq 'SET' ) {
 	my $old_local_udp = $cfg->returnOrigValue('l2tp-session local-udp-port');
 	my $old_peer_udp = $cfg->returnOrigValue('l2tp-session remote-udp-port');
 
-	my $old_cookie = " ";
-	my $old_port = " ";
+	my $old_cookie = [];
+	my $old_port   = [];
 
-	$old_port = "udp_sport $old_local_udp udp_dport $old_peer_udp"
+	$old_port = [ "udp_sport", $old_local_udp, "udp_dport", $old_peer_udp ]
 	    if defined($old_local_udp);	
-	$old_cookie = "cookie $old_local_cookie peer_cookie $old_peer_cookie"
+	$old_cookie =
+	  [ "cookie", $old_local_cookie, "peer_cookie", $old_peer_cookie ]
 	    if defined($old_local_cookie and $old_peer_cookie);
 
 	# Delete and recreate the tunnel only if the l2tp-session parameters change.
@@ -98,7 +99,7 @@ if ( $action eq 'SET' ) {
 	if ( ($tunnel ne $old_tunnel) or ($peer_tunnel ne $old_peer_tunnel) or
 	     ($session ne $old_session) or ($peer_session ne $old_peer_session) or
 	     ($local_ip ne $old_local_ip) or ($peer_ip ne $old_remote_ip) or 
-	     ($port ne $old_port) or ($cookie ne $old_cookie) or 
+	     ("@$port" ne "@$old_port" ) or ("@$cookie" ne "@$old_cookie") or 
 	     ($encap ne $old_encap) ) {
 		delete_tun_session($old_session);
 		add_new_session($tunnel, $peer_tunnel, $encap, $local_ip, $peer_ip,
@@ -111,10 +112,11 @@ exit 0;
 
 sub delete_tun_session {
     my $old_session = shift;
-    system("ip l2tp del session tunnel_id $old_session session_id $old_session") == 0
-	or exit 1;
-    system("ip l2tp del tunnel tunnel_id $old_session") == 0
-	or exit 1;
+    system( "ip", "l2tp", "del", "session", "tunnel_id", $old_session,
+        "session_id", $old_session ) == 0
+      or exit 1;
+    system( "ip", "l2tp", "del", "tunnel", "tunnel_id", $old_session ) == 0
+      or exit 1;
 }
 
 sub add_new_session {
@@ -133,10 +135,19 @@ sub add_new_session {
         Vyatta::Address::ipv6_tentative_addr_dad_wait( $$if_name, $local_ip );
     }
 
-    system("ip l2tp add tunnel tunnel_id $tunnel peer_tunnel_id $peer_tunnel encap $encap local $local_ip remote $peer_ip $port") == 0
-	or exit 1;
+    system(
+        "ip",        "l2tp",   "add",            "tunnel",
+        "tunnel_id", $tunnel,  "peer_tunnel_id", $peer_tunnel,
+        "encap",     $encap,   "local",          $local_ip,
+        "remote",    $peer_ip, @$port
+      ) == 0
+      or exit 1;
 
-    system("ip l2tp add session tunnel_id $tunnel session_id $session peer_session_id $peer_session $cookie name $ifname l2spec_type none") == 0
-	or exit 1;
-
+    system(
+        "ip",              "l2tp",        "add",        "session",
+        "tunnel_id",       $tunnel,       "session_id", $session,
+        "peer_session_id", $peer_session, "name",       $ifname,
+        "l2spec_type",     "none",        @$cookie
+      ) == 0
+      or exit 1;
 }
